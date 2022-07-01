2c6c1a667eaa9de91eb9d9febe29f037721d6db4
BuildTools <unconfigured@null.spigotmc.org>
2022-07-01 16:19:47 +0200

    🗽 Free old image buffer after displaying

--- lib/texturerender.dart
+++ lib/texturerender.dart
@@ -2,19 +2,29 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ffi/ffi.dart' as ffi;
 import 'dart:ffi' as ffi;
 
 class Texturerender {
+  static const MethodChannel _channel = MethodChannel('texturerender');
+  int id;
+  ffi.Pointer<ffi.Uint8> _previousFrame = ffi.nullptr;
   final ValueNotifier<int?> textureId = ValueNotifier<int?>(null);
 
-  static const MethodChannel _channel = MethodChannel('texturerender');
+  Texturerender({required this.id, required void Function() onRegistered}) {
+    _registerTexture().then((_) => onRegistered());
+  }
+
+  dispose() {
+    _unregisterTexture();
+  }
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  Future<void> getTextureId(int id) async {
+  Future<void> _registerTexture() async {
     textureId.value = await _channel.invokeMethod(
       "RegisterTexture",
       {
@@ -30,5 +40,16 @@ class Texturerender {
       "height": height,
       "buffer": buffer.address,
     });
+    if (_previousFrame != ffi.nullptr) ffi.calloc.free(_previousFrame);
+    _previousFrame = buffer;
+  }
+
+  Future<void> _unregisterTexture() async {
+    await _channel.invokeMethod(
+      "UnregisterTexture",
+      {
+        "id": id,
+      },
+    );
   }
 }
